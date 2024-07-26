6b13a23c5c390d18428e9b81837cfd05e3dc6229
HyperSoWeak <hypersoweak@gmail.com>
2024-07-27 01:47:19 +0800

    .

--- abc359/d.cpp
+++ abc359/d.cpp
@@ -13,7 +13,6 @@ using namespace std;
 #define MOD 998244353
 
 int k, n, a[1024][1024];
-bool ok[1024][1024];
 string s, ss = "AB";
 
 bool check(int l, int t) {
@@ -29,23 +28,20 @@ bool check(int l, int t) {
 }
 
 int dp(int l, int t) {
-    if(ok[l][t]) return a[l][t];
+    if(a[l][t] != -1) return a[l][t];
     if(!check(l,t)) {
-        ok[l][t] = 1, a[l][t] = 0;
+        a[l][t] = 0;
         return 0;
     }
     a[l][t] = (dp(l-1, (t>>1)) + dp(l-1, (t>>1)|(1<<(k-1)))) % MOD;
-    ok[l][t] = 1;
     return a[l][t];
 }
 
 signed main() {
     hyper;
     cin >> n >> k >> s;
-    rep(i,0,1<<k) {
-        ok[k][i] = 1;
-        if(check(k,i)) a[k][i] = 1;
-    }
+    rr(a,-1);
+    rep(i,0,1<<k) a[k][i] = check(k,i);
     int ans = 0;
     rep(i,0,1<<k) ans = (ans + dp(n,i)) % MOD;
     cout << ans;
