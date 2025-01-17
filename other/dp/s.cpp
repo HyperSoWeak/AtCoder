af079a1eb4321ff4b8337b645309162b9eb5e7d7
Hyper <hypersoweak@gmail.com>
2025-01-18 00:10:04 +0800

    .

--- other/dp/s.cpp
+++ other/dp/s.cpp
@@ -15,10 +15,27 @@ using vi = vector<int>;
 using mat = vector<vi>;
 const int IINF = 0x3f3f3f3f;
 const int INF = 0x3f3f3f3f3f3f3f3f;
-const int MOD = 998244353;
+const int MOD = 1e9+7;
 const int MN = 2e5+5;
+
+string k;
+int d, n;
+int dp[10005][105][2];
+
+int sol(int pos, int sum, int lim) {
+    if(pos == n) return sum == 0;
+    int& ret = dp[pos][sum][lim];
+    if(~ret) return ret;
+    ret = 0;
+    int up = lim ? k[pos] - '0' : 9;
+    rep1(i,0,up) ret += sol(pos+1, (sum+i)%d, lim&&(i==up));
+    return ret %= MOD;
+}
 
 signed main() {
     hyper;
-    
+    rr(dp, -1);
+    cin >> k >> d;
+    n = k.size();
+    cout << (sol(0, 0, 1) - 1 + MOD) % MOD << '\n';
 }
