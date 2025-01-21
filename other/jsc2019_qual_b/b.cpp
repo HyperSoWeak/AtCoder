7a304a313f0691d3f47105fcfce9cf3d13fae295
Hyper <hypersoweak@gmail.com>
2025-01-21 21:34:30 +0800

    .

---
+++ other/jsc2019_qual_b/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rep1(i,a,b) for(int i=a; i<=b; ++i)
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define max max<int>
+#define min min<int>
+#define rr(a,x) memset(a,x,sizeof(a))
+#define hyper ios::sync_with_stdio(0); cin.tie(0)
+using namespace std;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using mat = vector<vi>;
+const int IINF = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+const int MN = 2005;
+
+int n, k, a[MN];
+
+signed main() {
+    hyper;
+    cin >> n >> k;
+    rep(i,0,n) cin >> a[i];
+    int x = 0, y = 0;
+    rep(i,0,n) rep(j,i+1,n) if(a[i] > a[j]) ++x;
+    rep(i,0,n) rep(j,0,n) if(a[i] > a[j]) ++y;
+    cout << (x*k%MOD + k*(k-1)/2%MOD*y%MOD) % MOD << '\n';
+}
